6e0fc8aef06fe7b90c9a7b4b15201980005e40d8
sofyan48 <meongbego@gmail.com>
2024-11-13 10:10:10 +0700

    Add: Detail Service with all option

--- src/cmd/rollout/detail.go
+++ src/cmd/rollout/detail.go
@@ -2,6 +2,7 @@ package rollout
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"strings"
 	"time"
@@ -21,53 +22,71 @@ type Detail struct {
 }
 
 func (i *cmdRollout) detail(ctx context.Context) *cobra.Command {
+	var allOption bool
 	command := &cobra.Command{
 		Use:   "detail",
 		Short: "Service detail",
 		Run: func(c *cobra.Command, args []string) {
 			namespace, _ := c.Flags().GetString("namespace")
 			service, _ := c.Flags().GetString("service")
+
 			data, err := i.roll.SetNamepace(namespace).Detail(service)
 			if err != nil {
 				log.Fatalf("Error get list: ", err)
 			}
 
-			dataStructured := []Detail{}
-			for _, i := range data.ReplicaSets {
-				detailStructured := Detail{}
-				for _, p := range i.Pods {
-					detailStructured.Ready = p.Ready
-					detailStructured.Pod = p.ObjectMeta.Name
-					detailStructured.Status = p.Status
-					detailStructured.Created = &p.ObjectMeta.CreationTimestamp
-				}
-				for _, c := range i.Images {
-					detailStructured.Images = strings.Split(c, ":")[1]
-				}
-				detailStructured.Name = i.ObjectMeta.Name
-				dataStructured = append(dataStructured, detailStructured)
-			}
-
 			headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
 			columnFmt := color.New(color.FgYellow).SprintfFunc()
 
-			tbl := table.New("No", "Name", "Pods", "Ready", "Status", "Created", "Version")
+			tbl := table.New("Name", "Pods", "Status", "Available", "Desired", "Current", "Ready", "Version")
 			tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
-			for k, v := range dataStructured {
-				k += 1
-				tbl.AddRow(k,
-					v.Name,
-					v.Pod,
-					v.Ready,
-					v.Status,
-					v.Created,
-					v.Images,
-				)
-			}
+			tbl.AddRow(
+				data.ObjectMeta.Name,
+				data.ReplicaSets[0].Pods[0].ObjectMeta.Name,
+				data.ReplicaSets[0].Pods[0].Status,
+				data.ReplicaSets[0].Available,
+				data.Desired,
+				data.Current,
+				data.ReplicaSets[0].Pods[0].Ready,
+				strings.Split(data.ReplicaSets[0].Images[0], ":")[1],
+			)
 			tbl.Print()
+			if allOption {
+				fmt.Println("\nShow All resource")
+				tblAll := table.New("No", "Name", "Pods", "Status", "Ready", "Created", "Version")
+				tblAll.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+				dataStructured := []Detail{}
+				for _, i := range data.ReplicaSets {
+					detailStructured := Detail{}
+					for _, p := range i.Pods {
+						detailStructured.Ready = p.Ready
+						detailStructured.Pod = p.ObjectMeta.Name
+						detailStructured.Status = p.Status
+						detailStructured.Created = &p.ObjectMeta.CreationTimestamp
+					}
+					for _, c := range i.Images {
+						detailStructured.Images = strings.Split(c, ":")[1]
+					}
+					detailStructured.Name = i.ObjectMeta.Name
+					dataStructured = append(dataStructured, detailStructured)
+				}
+				for k, v := range dataStructured {
+					k += 1
+					tblAll.AddRow(k,
+						v.Name,
+						v.Pod,
+						v.Status,
+						v.Ready,
+						v.Created,
+						v.Images,
+					)
+				}
+				tblAll.Print()
+			}
 		},
 	}
 	command.Flags().StringP("namespace", "n", "", "Your namespace")
 	command.Flags().StringP("service", "s", "", "Your service")
+	command.Flags().BoolVarP(&allOption, "all", "a", false, "Show all items")
 	return command
 }
